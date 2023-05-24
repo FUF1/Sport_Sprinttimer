83d86330e4a9418135d7beca4cebdbd427f307eb
FUF1 <69962736+FUF1@users.noreply.github.com>
2023-05-24 09:16:18 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,27 @@
+let timeStart = 0
+let timeDelta = 0
+let timeEnd = 0
+input.onPinReleased(TouchPin.P0, function () {
+    timeStart = input.runningTime()
+    basic.showIcon(IconNames.SmallDiamond)
+    // Debouncing
+    basic.pause(100)
+})
+input.onButtonPressed(Button.AB, function () {
+    timeStart = 9999999999999
+    timeStart = 9999999999999
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    timeDelta = timeEnd - timeStart
+    basic.showNumber(timeDelta)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    timeEnd = input.runningTime()
+    basic.showIcon(IconNames.Square)
+    // Debouncing
+    basic.pause(500)
+})
+basic.forever(function () {
+	
+})
